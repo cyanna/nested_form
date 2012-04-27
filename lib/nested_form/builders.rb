3b0f6d3611d5fdb4c3b3816dfe9e27ea885abac2
Chris Irish <supairish@gmail.com>
2012-04-27 00:21:09 -0700

    made Edvera needed changes so we can remove vendored version

--- lib/nested_form/builders.rb
+++ lib/nested_form/builders.rb
@@ -1,10 +1,10 @@
 require 'nested_form/builder_mixin'
-
+ 
 module NestedForm
-  class Builder < ::ActionView::Helpers::FormBuilder
+  class Builder < ::StandardFormBuilder
     include ::NestedForm::BuilderMixin
   end
-
+ 
   begin
     require 'simple_form'
     class SimpleBuilder < ::SimpleForm::FormBuilder
@@ -12,7 +12,7 @@ module NestedForm
     end
   rescue LoadError
   end
-
+ 
   begin
     require 'formtastic'
     class FormtasticBuilder < (defined?(::Formtastic::FormBuilder) ? Formtastic::FormBuilder : ::Formtastic::SemanticFormBuilder)
